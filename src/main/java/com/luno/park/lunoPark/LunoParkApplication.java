cadef0144c850caed94878c545a6dc52453761cc
Sgavr1 <sanya.gavrilko60@gmail.com>
2023-11-04 10:32:11 +0200

    Start

---
+++ src/main/java/com/luno/park/lunoPark/LunoParkApplication.java
@@ -0,0 +1,13 @@
+package com.luno.park.lunoPark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunoParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunoParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luno/park/lunoPark/LunoParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luno.park.lunoPark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunoParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
